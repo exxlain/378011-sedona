4cc10630a1fd9f8b6f424db8b3c017f45060a11f
Lana Kostina <exxlain@gmail.com>
2017-09-12 20:38:59 +0700

    анимация

--- script.js
+++ script.js
@@ -35,3 +35,11 @@ var login = popup.querySelector("[name=date]");
       }
     }
   });
+
+
+/*
+var iframeMap = <iframe> src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52385.180521669856!2d-111.83024387662934!3d34.8543733563858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872da132f942b00d%3A0x5548c523fa6c8efd!2z0KHQtdC00L7QvdCwLCDQkNGA0ZbQt9C-0L3QsCA4NjMzNiwg0KHQv9C-0LvRg9GH0LXQvdGWINCo0YLQsNGC0Lgg0JDQvNC10YDQuNC60Lg!5e0!3m2!1suk!2sua!4v1505219318423" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>;
+var map = document.querySelector('.map-wrapper');
+map.innerHtml = iframeMap;
+*/
+
