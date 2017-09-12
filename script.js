d90de3b540f3de2d6fb0cde530f5e9e816c96d72
Lana Kostina <exxlain@gmail.com>
2017-09-12 19:22:51 +0700

    работа после проверки
    Д19 - не получилось в формеЖ взрослые и дети,
    б13 - частично

--- script.js
+++ script.js
@@ -15,14 +15,15 @@ var login = popup.querySelector("[name=date]");
    login.focus();
   });
 
+
+
   popup.addEventListener("submit", function (evt) {
        if (!date.value || !date2.value) {
       evt.preventDefault();
       popup.classList.remove("modal-error");
       popup.offsetWidth = popup.offsetWidth;
       popup.classList.add("modal-error");
-      console.log("Нужно ввести дату заезда и дату выезда");
-           
+      console.log("Нужно ввести дату заезда и дату выезда");     
     }
   });
 
@@ -31,29 +32,6 @@ var login = popup.querySelector("[name=date]");
       if (popup.classList.contains("modal-show")) { 
           popup.classList.remove("modal-show");
           popup.classList.add("modal-close");
-      }
-    }
-  });
-  var mapLink = document.querySelector(".map-wrapper");
-
-  var mapPopup = document.querySelector(".modal-map");
-/*  var mapClose = mapPopup.querySelector(".modal-close");*/
-
-mapLink.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    mapPopup.classList.add("modal-show");
-  });
-
- /* mapClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    mapPopup.classList.remove("modal-show");
-  });*/
-
-
-  window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      if (mapPopup.classList.contains("modal-show")) {
-        mapPopup.classList.remove("modal-show");
       }
     }
   });
